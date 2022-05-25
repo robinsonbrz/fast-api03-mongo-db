fd87c5ff15d77c87b840d3421acea5d3d5d68b20
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-25 20:34:38 -0300

    Pastas, Config database, Dot env criado

---
+++ config/database.py
@@ -0,0 +1,18 @@
+import os
+from email.mime import application
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+
+USER = os.getenv('MONGODB_USER')
+PASSW = os.getenv('MONGODB_PASS')
+
+client = MongoClient(f"mongodb+srv://{USER}:{PASSW}@cluster0.0nnjfk0.mongodb.net/?retryWrites=true&w=majority")
+
+db = client.todo_app
+
+collection_name = db["todos_app"]
+
