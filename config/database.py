c82aa586a7c63974c5c689601bd29a8a6a333939
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-25 23:46:47 -0300

    env variables

--- config/database.py
+++ config/database.py
@@ -1,4 +1,5 @@
 import os
+import os.path
 from email.mime import application
 
 from dotenv import load_dotenv
@@ -7,10 +8,11 @@ from pymongo import MongoClient
 load_dotenv()
 
 
-# USER = os.getenv('MONGODB_USER')
-# PASSW = os.getenv('MONGODB_PASS')
+if os.path.exists('.env'):
+    MONGODB_USER = os.getenv('MONGODB_USER')
+    MONGODB_PASS = os.getenv('MONGODB_PASS')
 
-client = MongoClient(f"mongodb+srv://os.getenv('MONGODB_USER'):os.getenv('MONGODB_PASS')@cluster0.0nnjfk0.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient(f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASS}@cluster0.0nnjfk0.mongodb.net/?retryWrites=true&w=majority")
 
 db = client.todo_app
 
