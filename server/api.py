e08a129a09dd655afaf9e57369b93a9dc3c1f69a
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-27 12:24:15 -0300

    Included CORS

--- server/api.py
+++ server/api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from routes.todos_route import todo_api_router
 
 app = FastAPI(
@@ -12,5 +12,24 @@ app = FastAPI(
         "email": "robinsonbrz@gmail.com",
         },
     )
+
+origins = [
+    "https://fast-api03-mongo-db.vercel.app",
+    "http://localhost",
+    "http://localhost:8080",
+    "0.0.0.0/0",
+    "*"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 app.include_router(todo_api_router)
